b641094bc30e83eae40bc1effd6d32c42360f538
Alli_anne <alannieprice@gmail.com>
2024-10-18 22:27:45 -0600

    change

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
-        conditions: resolve(__dirname, "src/condtions.html")
+        conditions: resolve(__dirname, "src/conditions.html")
       }
     }
   }
