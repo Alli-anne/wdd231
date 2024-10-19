4226ec710e0b57ecb3bc566586ef9071f225d74f
Alli_anne <alannieprice@gmail.com>
2024-10-18 21:59:14 -0600

    help me crying

--- src/js/parkService.mjs
+++ src/js/parkService.mjs
@@ -201,7 +201,7 @@ const parkInfoLinks = [
 ];
 
 const baseUrl = "https://developer.nps.gov/api/v1/";
-const apiKey = "A8AtTg2KI95rU5Ln3FCOkcTHFlePlfOd8WmxjBFn";
+const apiKey = import.meta.env.VITE_NPS_API_KEY;
 
 async function getJson(url) {
   const options = {
